00df5a3fdb134c6d4b9f90d8f636bb5f375e4691
Dean <mccarthey123@outlook.com>
2018-08-22 18:50:07 +0800

    add event

--- src/components/note.js
+++ src/components/note.js
@@ -3,30 +3,46 @@ import './note.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Checkbox from '@material-ui/core/Checkbox';
+// import ListItem from '@material-ui/core/ListItem';
+// import ListItemText from '@material-ui/core/ListItemText';
+// import Checkbox from '@material-ui/core/Checkbox';
 
 class Note extends Component {
   state = {
 		checked: [0],
-		toDoList: [0, 1, 2, 3, 4],
+		itemToAdd: '',
+		toDoList: [0,1,2,3,4],
   };
   
   handleToggle = value => () => {
-	const { checked } = this.state;
-	const currentIndex = checked.indexOf(value);
-	const newChecked = [...checked];
+		const { checked } = this.state;
+		const currentIndex = checked.indexOf(value);
+		const newChecked = [...checked];
+		
+		if(currentIndex === -1) {
+			newChecked.push(value);
+		} else {
+			newChecked.splice(currentIndex, 1);
+		}
+		
+		this.setState({
+			checked: newChecked,
+		})
+	};
 	
-	if(currentIndex === -1) {
-		newChecked.push(value);
-	} else {
-		newChecked.splice(currentIndex, 1);
-	}
+	handleAddItem = () => {
+		console.log(this.state.itemToAdd)
+		if (!this.state.itemToAdd) return false;
+		this.setState({
+			toDoList: this.state.toDoList.push(Number(this.state.itemToAdd))
+		})
+		console.log(this.state.toDoList)
+	};
 	
-	this.setState({
-		checked: newChecked,
-	})
+	handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
   };
   
   render() {
@@ -35,29 +51,31 @@ class Note extends Component {
 		<div className="note-add-input">
 			<TextField
 			label="note"
-			defaultValue="write something"
+			onChange={this.handleChange('itemToAdd')}
+			value={this.state.itemToAdd}
 			margin="normal"
 			/>
-			<Button variant="contained" color="primary" className='note-add-btn'>
+			<Button onClick={this.handleAddItem} variant="contained" color="primary" className='note-add-btn'>
 				Add
 			</Button>
 		</div>
 		<div className="note-list">
 			<List>
 			{this.state.toDoList.map(value => (
-				<ListItem
-				key={value}
-				dense
-				button
-				onClick={this.handleToggle(value)}
-				>
-				<Checkbox
-					checked={this.state.checked.indexOf(value) !== -1}
-					tabIndex={-1}
-					disableRipple
-				/>
-				<ListItemText primary={`Line item ${value + 1}`} />
-				</ListItem>
+				// <ListItem
+				// key={value}
+				// dense
+				// button
+				// onClick={this.handleToggle(value)}
+				// >
+				// <Checkbox
+				// 	checked={this.state.checked.indexOf(value) !== -1}
+				// 	tabIndex={-1}
+				// 	disableRipple
+				// />
+				// <ListItemText primary={`Line item ${value + 1}`} />
+				// </ListItem>
+				<p key={value}>{value}</p>
 			))}
 			</List>
 		</div>
