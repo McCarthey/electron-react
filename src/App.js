b959f1319a595631a183d3a354b530dfb937f061
Dean <mccarthey123@outlook.com>
2018-08-23 17:40:16 +0800

    react hello

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import Note from './components/note.js'
 import WbSunny from '@material-ui/icons/WbSunny'
+
+// for test and practice
+import HelloMessage from './test/hello'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <WbSunny className="App-logo" style={{ fontSize: 64, color: '#f00' }} />
+          <HelloMessage name='McCarthey' />
           <h1 className="App-title">Have a nice day!</h1>
         </header>
         <Note />
