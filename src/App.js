5ac11cd1df5c5df06b747bf1c98b05bdf4b49f84
Dean <mccarthey123@outlook.com>
2018-08-31 18:49:36 +0800

    add react-router v4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+  // Switch 
+} from 'react-router-dom'
 import './App.css';
 import Note from './components/note.js'
 import WbSunny from '@material-ui/icons/WbSunny'
@@ -18,8 +24,15 @@ class App extends Component {
           <h1 className="App-title">Have a nice day!</h1>
           <Timer />
         </header>
-        <Note />
-        <MDConverter />
+        <Router>
+          <div>
+            <span><Link to="/Note">Note</Link></span>
+            <br />
+            <span><Link to="/md-converter">MD</Link></span>
+            <Route path="/Note" component={Note}/>
+            <Route path='/md-converter' component={MDConverter}/>
+          </div>
+        </Router>
       </div>
     );
   }
