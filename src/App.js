c864cebfaae7f90e9997a5fc6f699713d45de3be
Dean <mccarthey123@outlook.com>
2018-09-03 17:26:08 +0800

    jsx vscode formatting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 import {
-  BrowserRouter as Router,
-  // HashRouter as Router,
-  Route,
-  Link
-  // Switch 
+    BrowserRouter as Router,
+    // HashRouter as Router,
+    Route,
+    Link
+    // Switch
 } from 'react-router-dom'
-import './App.css';
+import './App.css'
 import Note from './components/note.js'
 import NameDialog from './components/nameDialog.js'
 import WbSunny from '@material-ui/icons/WbSunny'
@@ -18,46 +18,50 @@ import MDConverter from './test/md-convert'
 import TodoApp from './test/todo'
 
 class App extends Component {
-  state = {
-    username: ''
-  }
-  
-  componentDidMount() {
-    const username = localStorage.getItem('RE_toDoList_name')
-    if (username) {
-      this.setState({ username })
+    state = {
+        username: ''
     }
-  }
-  
-  render() {
-    let dialog
-    if (!this.state.username) {
-      dialog = <NameDialog />
+
+    componentDidMount() {
+        const username = localStorage.getItem('RE_toDoList_name')
+        if (username) {
+            this.setState({username})
+        }
     }
-    
-    return (
-      <div className="App">
-        <header className="App-header">
-          <WbSunny className="App-logo" style={{ fontSize: 64, color: '#f00' }} />
-          <HelloMessage name={this.state.username} />
-          <h1 className="App-title">Have a nice day!</h1>
-          <Timer />
-        </header>
-        {dialog}
-        <Router>
-          <div>
-            <span><Link to="/Note">Note</Link></span>
-            <br />
-            <span><Link to="/md-converter">MD</Link></span>
-            <Route exact path="/" component={Note}/>
-            <Route path="/Note" component={Note}/>
-            <Route path='/md-converter' component={MDConverter}/>
-            <Route path='/test' component={TodoApp}/>
-          </div>
-        </Router>
-      </div>
-    );
-  }
+
+    render() {
+        let dialog
+        if (!this.state.username) {
+            dialog = <NameDialog />
+        }
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <WbSunny className="App-logo" style={{fontSize: 64, color: '#f00'}} />
+                    <HelloMessage name={this.state.username} />
+                    <h1 className="App-title">Have a nice day!</h1>
+                    <Timer />
+                </header>
+                {dialog}
+                <Router>
+                    <div>
+                        <span>
+                            <Link to="/Note">Note</Link>
+                        </span>
+                        <br />
+                        <span>
+                            <Link to="/md-converter">MD</Link>
+                        </span>
+                        <Route exact path="/" component={Note} />
+                        <Route path="/Note" component={Note} />
+                        <Route path="/md-converter" component={MDConverter} />
+                        <Route path="/test" component={TodoApp} />
+                    </div>
+                </Router>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
